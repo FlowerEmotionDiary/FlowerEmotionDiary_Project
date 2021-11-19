a29e16bbe31acc6de77b6c568c140c1785d030e8
moonjiyoon <ske0830@gmail.com>
2021-11-19 15:58:31 +0900

    feat: login

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Login from './Login';
+// import EventPractice from './EventPractice';
+// import Say from './Say';
+
+const App = () => {
+  // return <Say />;
+  return <Login />;
+}
+
+export default App;
