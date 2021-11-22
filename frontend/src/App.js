5fa049a9d00ce1267078ebd301987dc7fe20cd6e
moonjiyoon <ske0830@gmail.com>
2021-11-22 19:20:55 +0900

    feat: Calendar page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
-import Login from './Login';
+// import Login from './Login';
 // import EventPractice from './EventPractice';
 // import Say from './Say';
+import CalendarPage from './Calendar';'./pages/CalendarPage';
+
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const App = () => {
   // return <Say />;
-  return <Login />;
+  // return <Login />;
+  // return <CalendarPage />;
+
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/calendar" component={CalendarPage} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+    
+  );
 }
 
 export default App;
