25a1cf5c0c4d66ec23fdae73a8bd6120055bd083
최유림 <choiyoolim@choeyulim-ui-MacBookPro.local>
2021-11-23 03:42:43 +0900

    feat: 다이어리 작성에 axios

--- frontend/src/DiaryWrite.js
+++ frontend/src/DiaryWrite.js
@@ -1,43 +1,82 @@
 import { useState } from "react";
 import ReactDOM from "react-dom";
+// import { useNavigate } from "react-router-dom";
 import TopTitle from "./components/login-topSection/TopTitle";
-
-// const axios = require('axios');
+import axios from "axios";
+import { Button, Form, FormField, Input } from 'semantic-ui-react';
 
 const DiaryWrite = () => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const [date, setDate] = useState(new Date()); // 초기 상태 : 현재 날짜와 시간으로 설정
 
-    const handleChange = (event) => {
-        setContent(event.target.value)
+    const handleDateChange = (e) => {
+        setDate(new Date(e.target.value));
+        console.log(date);
+      };
+
+    const handleChange = (e) => {
+        setContent(e.target.value);
+    }
+
+    const submitDiary = () => {
+        const year = date.getFullYear().toString();
+        const month = (date.getMonth()+1).toString();
+        const day = date.getDate().toString();
+        const whatDay = `${year}-${month >= 10 ? month : '0' + month}-${day >= 10 ? day : '0' + day}`;
+        console.log(whatDay);
+        const diaryData = {title:title, content: content, date: whatDay};
+        axios.post('http://localhost:5000/api/diary', diaryData).then((res) => {
+        console.log(res)
+        })
     }
     
     return (
         <>
         <TopTitle />
         <div className="Diary">
-            <form>
-                제목 : <input
-                    className="DiaryTitle"
-                    type="text"
-                    name="DiaryTitle"
-                    value={title}
-                    placeholder="제목을 입력하세요."
-                    onChange={
-                        (e) => {
-                            // console.log(e.target.value)
-                            setTitle(e.target.value)
+            <Form>
+                <FormField>
+                    제목 : <Input
+                        className="DiaryTitle"
+                        type="text"
+                        name="DiaryTitle"
+                        value={title}
+                        placeholder="제목을 입력하세요."
+                        onChange={
+                            (e) => {
+                                // console.log(e.target.value)
+                                setTitle(e.target.value)
+                            }
                         }
-                    }
-                    style={{width : "700px", resize : "none"}}
-                /><br />
-                내용 : <br /><textarea className="DiaryContent" value={content} 
-                placeholder="내용을 입력하세요" onChange={handleChange} style={{width : "700px", height : "500px", resize : "none"}} /><br />
-                {/* <textarea className="DiaryContent" value={content} 
-                placeholder="내용을 입력하세요" onChange={handleChange} /><br /> */}
-                {/* resize : 크기 고정 */}
-                <button type="submit" className="DiaryWriteButton">저장</button>
-            </form>
+                        style={{width : "700px", resize : "none"}}
+                    />
+                </FormField>
+                <FormField>
+                    날짜 : <Input 
+                    type="date" 
+                    name="date" 
+                    value={date} 
+                    onChange={handleDateChange}
+                    style={{width : "700px", resize : "none"}} 
+                    />
+                </FormField>
+                <FormField>
+                    내용 : <br />
+                    <textarea 
+                    className="DiaryContent" 
+                    value={content} 
+                    placeholder="내용을 입력하세요" 
+                    onChange={handleChange} 
+                    style={{width : "700px", height : "500px", resize : "none"}} 
+                    />
+                </FormField>
+                <FormField>
+                    <Button type="submit" className="DiaryWriteButton" onClick={submitDiary}>
+                        저장
+                    </Button>
+                </FormField>
+            </Form>
         </div>
         </>
     );
