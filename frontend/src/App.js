bd5d55cd81a3d6efd3aff8794ee2406b8d3cb32e
최유림 <choiyoolim@choeyulim-ui-MacBookPro.local>
2021-11-25 05:45:43 +0900

    feat: Route를 포함한 App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,51 @@
-
 import "./App.css";
 import CalendarPage from "./pages/CalendarPage";
-import DiaryWrite from "./pages/DiaryWritePage";
+import DiaryWritePage from "./pages/DiaryWritePage";
 import DiaryPage from "./pages/DiaryPage";
+// import date from "./pages/DiaryWritePage";
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
+  // const diaryResultPath = `/diary/${date}`
   return (
     <div className="App">
-      <Router>
         <Routes>
-          <Route exact path="/calendar" element={<CalendarPage />} />
-          <Route exact path="/diary-write" element={<DiaryWrite />} />
-          <Route exact path="/diary/:date" element={<DiaryPage />} />
+          <Route  path="/calendar" element={<CalendarPage />} />
+          <Route  path="/diary-write" element={<DiaryWritePage />} /> {/* 일기 작성 페이지 */}
+          <Route  path="/diary/:date" element={<DiaryPage />} /> {/* 일기 보여주는 페이지 */}
+          <Route path="/" element={<Navigate to="/diary-write" />} />
         </Routes>
-      </Router>
     </div>
   );
 };
 
 export default App;
+
+
+// import "./App.css";
+// import CalendarPage from "./pages/CalendarPage";
+// import DiaryWrite from "./pages/DiaryWritePage";
+// import DiaryPage from "./pages/DiaryPage";
+
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<Navigate to="/diary-write" />} />
+//           <Route  path="/calendar" element={<CalendarPage />} />
+//           <Route  path="/diary-write" element={<DiaryWrite />} />
+//           <Route  path="/diary/:date" element={<DiaryPage />} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// };
+
+// export default App;
 
