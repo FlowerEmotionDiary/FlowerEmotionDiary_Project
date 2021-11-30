8fab2454f442aaf2954f30ec99dc4f0209dc6dff
최유림 <choiyoolim@choeyulim-ui-MacBookPro.local>
2021-11-30 16:08:23 +0900

    feat: 다이어리 작성 페이지 날짜 넘기기 완료

--- frontend/src/pages/DiaryWritePage.js
+++ frontend/src/pages/DiaryWritePage.js
@@ -1,34 +1,25 @@
+// 참고 : https://explain-programming.tistory.com/5
+
 import { useState } from "react";
-// import ReactDOM from "react-dom";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import TopTitle from "../components/login-topSection/TopTitle";
 import axios from "axios";
 import { Button, Form, FormField, Input } from 'semantic-ui-react';
-// import { Link } from 'react-router-dom';
-// import DiaryPage from "./DiaryPage";
-// import { BrowserRouter, useNavigate } from 'react-router-dom';
-// import { useNavigate } from "react-router";
-// import App from "../App";
 
-const DiaryWritePage = props => {
+const DiaryWritePage = () => {
+    let location = useLocation(); // useLocation 훅은 현재의 URL을 대표하는 location 객체를 반환
+    const params = new URLSearchParams(location.search);
+    const whatDay = params.get('selectedDate');
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
-    // const [date, setDate] = useState(new Date()); // 초기 상태 : 현재 날짜와 시간으로 설정
-
-    // const year = date.getFullYear().toString();
-    // const month = (date.getMonth()+1).toString();
-    // const day = date.getDate().toString();
-    const month = parseInt(props.month)
-    const day = parseInt(props.day)
-    const whatDay = `${props.year}-${month >= 10 ? month : '0' + month}-${day >= 10 ? day : '0' + day}`;
-    console.log(whatDay);
-
-    const [date, setDate] = useState(whatDay); // 초기 상태 : 현재 날짜와 시간으로 설정
-
-    // var navigate = useNavigate();
+    const [date, setDate] = useState(whatDay) // 초기 상태 : 현재 날짜와 시간으로 설정
 
     const handleDateChange = (e) => {
-        setDate(new Date(e.target.value));
+        const d = new Date(e.target.value);
+        const year = d.getFullYear().toString();
+        const month = (d.getMonth()+1);
+        const day = d.getDate();
+        setDate(`${year}-${month >= 10 ? month : '0' + month}-${day >= 10 ? day : '0' + day}`);
         console.log(date);
       };
 
@@ -37,17 +28,12 @@ const DiaryWritePage = props => {
     }
 
     const SubmitDiary = () => {
-        // let navigate = useNavigate();
-        // const year = date.getFullYear().toString();
-        // const month = (date.getMonth()+1).toString();
-        // const day = date.getDate().toString();
-        // const whatDay = `${year}-${month >= 10 ? month : '0' + month}-${day >= 10 ? day : '0' + day}`;
-        // console.log(whatDay);
-        const diaryData = {title:title, content: content, date: whatDay};
+        const diaryData = {title:title, content: content, date: date};
+        console.log("diary-data-date: ", date)
         axios.post('http://elice-kdt-2nd-team11.koreacentral.cloudapp.azure.com/api/diary', diaryData).then((res) => {
         console.log(res)
         })
-        navigate(`/diary/${whatDay}`);
+        navigate(`/diary/${date}`);
     }
 
     var navigate = useNavigate();
@@ -57,8 +43,6 @@ const DiaryWritePage = props => {
         <TopTitle />
         <div className="Diary">
             <Form onSubmit={SubmitDiary}>
-            {/* <Form onSubmit={navigate(`/diary/${whatDay}`)}> */}
-            {/* <Form> */}
                 <FormField>
                     제목 : <Input
                         className="DiaryTitle"
@@ -68,7 +52,6 @@ const DiaryWritePage = props => {
                         placeholder="제목을 입력하세요."
                         onChange={
                             (e) => {
-                                // console.log(e.target.value)
                                 setTitle(e.target.value)
                             }
                         }
@@ -96,7 +79,6 @@ const DiaryWritePage = props => {
                 </FormField>
                 <FormField>
                     <Button type="submit" className="DiaryWriteButton">저장</Button>
-                    {/* <Button type="submit" className="DiaryWriteButton">저장</Button> */}
                 </FormField>
             </Form>
         </div>
@@ -104,124 +86,4 @@ const DiaryWritePage = props => {
     );
 }
 
-// ReactDOM.render(
-//     <BrowserRouter>
-//         <App />
-//     </BrowserRouter>, 
-// document.getElementById('root')
-// );
-
 export default DiaryWritePage;
-
-
-// import { useState } from "react";
-// // import ReactDOM from "react-dom";
-// import { useNavigate } from "react-router-dom";
-// import TopTitle from "../components/login-topSection/TopTitle";
-// import axios from "axios";
-// import { Button, Form, FormField, Input } from 'semantic-ui-react';
-// // import { Link } from 'react-router-dom';
-// // import DiaryPage from "./DiaryPage";
-// // import { BrowserRouter, useNavigate } from 'react-router-dom';
-// // import { useNavigate } from "react-router";
-// // import App from "../App";
-
-// const DiaryWritePage = props => {
-//     const [title, setTitle] = useState('');
-//     const [content, setContent] = useState('');
-//     const [date, setDate] = useState(new Date(props.year, props.month, props.day)); // 초기 상태 : 현재 날짜와 시간으로 설정
-
-//     const year = date.getFullYear().toString();
-//     const month = (date.getMonth()+1).toString();
-//     const day = date.getDate().toString();
-//     const whatDay = `${year}-${month >= 10 ? month : '0' + month}-${day >= 10 ? day : '0' + day}`;
-//     console.log(whatDay);
-
-//     // var navigate = useNavigate();
-
-//     const handleDateChange = (e) => {
-//         console.log("e.target.value: ", e.target.value)
-//         setDate(new Date(e.target.value));
-//         console.log(date);
-//       };
-
-//     const handleChange = (e) => {
-//         setContent(e.target.value);
-//     }
-
-//     const SubmitDiary = () => {
-//         // let navigate = useNavigate();
-//         // const year = date.getFullYear().toString();
-//         // const month = (date.getMonth()+1).toString();
-//         // const day = date.getDate().toString();
-//         // const whatDay = `${year}-${month >= 10 ? month : '0' + month}-${day >= 10 ? day : '0' + day}`;
-//         // console.log(whatDay);
-//         const diaryData = {title:title, content: content, date: whatDay};
-//         axios.post('http://localhost:5000/api/diary', diaryData).then((res) => {
-//         console.log(res)
-//         })
-//         navigate(`/diary/${whatDay}`);
-//     }
-
-//     var navigate = useNavigate();
-    
-//     return (
-//         <>
-//         <TopTitle />
-//         <div className="Diary">
-//             <Form onSubmit={SubmitDiary}>
-//             {/* <Form onSubmit={navigate(`/diary/${whatDay}`)}> */}
-//             {/* <Form> */}
-//                 <FormField>
-//                     제목 : <Input
-//                         className="DiaryTitle"
-//                         type="text"
-//                         name="DiaryTitle"
-//                         value={title}
-//                         placeholder="제목을 입력하세요."
-//                         onChange={
-//                             (e) => {
-//                                 // console.log(e.target.value)
-//                                 setTitle(e.target.value)
-//                             }
-//                         }
-//                         style={{width : "700px", resize : "none"}}
-//                     />
-//                 </FormField>
-//                 <FormField>
-//                     날짜 : <Input 
-//                     type="date" 
-//                     name="date" 
-//                     value={date} 
-//                     onChange={handleDateChange}
-//                     style={{width : "700px", resize : "none"}} 
-//                     />
-//                 </FormField>
-//                 <FormField>
-//                     내용 : <br />
-//                     <textarea 
-//                     className="DiaryContent" 
-//                     value={content} 
-//                     placeholder="내용을 입력하세요" 
-//                     onChange={handleChange} 
-//                     style={{width : "700px", height : "500px", resize : "none"}} 
-//                     />
-//                 </FormField>
-//                 <FormField>
-//                     <Button type="submit" className="DiaryWriteButton">저장</Button>
-//                     {/* <Button type="submit" className="DiaryWriteButton">저장</Button> */}
-//                 </FormField>
-//             </Form>
-//         </div>
-//         </>
-//     );
-// }
-
-// // ReactDOM.render(
-// //     <BrowserRouter>
-// //         <App />
-// //     </BrowserRouter>, 
-// // document.getElementById('root')
-// // );
-
-// export default DiaryWritePage;
