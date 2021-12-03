2a77785484edb87bf413993550ad795a8ae36a83
최유림 <choiyoolim@choeyulim-ui-MacBookPro.local>
2021-12-03 16:13:22 +0900

    feat: Route 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,12 @@ import FirstPage from "./pages/FirstPage";
 import RegisterPage from "./pages/RegisterPage";
 import MoreAboutPage from "./pages/MoreAboutPage";
 import FlowerListPage from "./pages/FlowerListPage";
+import Book from "./components/Book/Book";
+import ShowCalendar from "./components/Book/ShowCalendar";
+import ShowDiaryWrite from "./components/Book/ShowDiaryWrite";
+import ShowFlowerList from "./components/Book/ShowFlowerList";
+import ShowMoreAbout from "./components/Book/ShowMoreAbout";
+import ShowDiaryPage from "./components/Book/ShowDiaryPage";
 
 const App = () => {
   const persistor = persistStore(store);
@@ -19,14 +25,20 @@ const App = () => {
   return (
     <Provider store={store}>
         <Routes>
-          <Route exact path="/" element={<FirstPage  />} />
-          <Route exact path="/calendar" element={<CalendarPage />} />
-          <Route exact path="/diary-write" element={<DiaryWritePage />} />
-          <Route exact path="/diary" element={<DiaryPage />} />
+          {/* <Route exact path="/book" element={<Book  />} /> */}
+          {/* <Route exact path="/calendar" element={<CalendarPage />} /> */}
+          {/* <Route exact path="/diary-write" element={<DiaryWritePage />} /> */}
+          {/* <Route exact path="/diary" element={<DiaryPage />} /> */}
           <Route exact path="/login" element={<LoginPage  />} />
           <Route exact path='/register' element={<RegisterPage />} />
-          <Route exact path='/moreAbout' element={<MoreAboutPage />} />
-          <Route exact path='/flowerList' element={<FlowerListPage />} />
+          {/* <Route exact path='/moreAbout' element={<MoreAboutPage />} /> */}
+          {/* <Route exact path='/flowerList' element={<FlowerListPage />} /> */}
+          {/* <Route exact path="/pressCalendar" element={<PressCalendar />} /> */}
+          <Route exact path="/calendar" element={<ShowCalendar />} />
+          <Route exact path="/diary-write" element={<ShowDiaryWrite />} />
+          <Route exact path='/flowerList' element={<ShowFlowerList />} />
+          <Route exact path='/moreAbout' element={<ShowMoreAbout />} />
+          <Route exact path="/diary" element={<ShowDiaryPage />} />
         </Routes>
     </Provider>
   );
