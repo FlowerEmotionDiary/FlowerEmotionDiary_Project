3cca5daeab42c5ffd7047216a4e4c44e981ac010
moonjiyoon <ske0830@gmail.com>
2021-12-06 03:24:57 +0900

    feat: Flower page에서 일기작성횟수, 최근감정 출력

--- frontend/src/components/FlowerList.js
+++ frontend/src/components/FlowerList.js
@@ -1,7 +1,25 @@
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+
 const FlowerList = () => {
+    const [count, setCount] = useState(0);
+    const [emotion, setEmotion] = useState("");
+    const getFlower = async () => {
+        const response = await axios.get('/flower')
+        console.log(response)
+        setCount(response.data.count);
+        setEmotion(response.data.emotion);
+    }
+    useEffect(() => {
+        getFlower();
+    }, []);
+
     return( 
         <>
         <h1>Flower List</h1>
+        <p>{count}</p>
+        <p>{emotion}</p>
         </>
     );
 }
