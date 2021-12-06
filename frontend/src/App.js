80312102db8156ed416b80c9ee894dee9657da00
최유림 <choiyoolim@choeyulim-ui-MacBookPro.local>
2021-12-06 19:02:56 +0900

    feat: ShowLoginPage & ShowRegisterPage 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
 import "./App.css";
-import LoginPage from "./login/LoginPage"
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import RegisterPage from "./pages/RegisterPage";
+import FirstPage from "./pages/FirstPage";
+import ShowRegisterPage from "./pages/ShowRegisterPage";
 import ShowCalendar from "./pages/ShowCalendar";
 import ShowDiaryWrite from "./pages/ShowDiaryWrite";
 import ShowFlowerList from "./pages/ShowFlowerList";
 import ShowMoreAbout from "./pages/ShowMoreAbout";
 import ShowDiaryPage from "./pages/ShowDiaryPage";
+import ShowLoginPage from "./pages/ShowLoginPage";
 
 const App = () => {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/login" element={<LoginPage  />} />
-        <Route exact path='/register' element={<RegisterPage />} />
+        <Route exact path="/" element={<FirstPage />} />
+        <Route exact path="/login" element={<ShowLoginPage  />} />
+        <Route exact path='/register' element={<ShowRegisterPage />} />
         <Route exact path="/calendar" element={<ShowCalendar />} />
         <Route exact path="/diary-write" element={<ShowDiaryWrite />} />
         <Route exact path='/flowerList' element={<ShowFlowerList />} />
