4b5a80693b69994fe2073275bb2f21b899718191
moonjiyoon <ske0830@gmail.com>
2021-12-09 11:26:18 +0900

    feat: 로그인, 회원가입 시 유효한 이메일, 비밀번호인지 체크
    Former-commit-id: 8ad978ce520604237c59559cd1c5515469dfb080

--- frontend/src/login/loginForm.js
+++ frontend/src/login/loginForm.js
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router';
 import { Formik } from "formik";
 import {loginDB} from "./LoginValidation";
 import "./LoginForm.scss";
+import { NavLink } from 'react-router-dom';
+import * as Yup from 'yup';
 
 const LoginForm = () => {
 	const navigate = useNavigate();
@@ -12,7 +14,14 @@ const LoginForm = () => {
 			<div className="logintitle">LOGIN</div>
 			<Formik 
 				initialValues={{ email: "", password: ""}}
-				// validationSchema={}
+				validationSchema={Yup.object({
+					email: Yup.string().email('잘못된 이메일 주소입니다.')
+						.required('이메일을 입력해주세요.'),
+					password: Yup.string()
+						.min(8, '비밀번호가 너무 짧습니다. 비밀번호는 최소 8자 이상이어야 합니다.')
+						.max(20, '비밀번호는 20자 이하여야 합니다.')
+						.required('비밀번호를 입력해주세요.'),
+				})}
 				onSubmit={(values, {setSubmitting}) => {
 					try {
 						loginDB(values, navigate);
@@ -25,18 +34,22 @@ const LoginForm = () => {
 						placeholder="  이메일을 입력하세요."  
 						{...formik.getFieldProps('email')} />
 						{ formik.touched.email && formik.errors.email ? 
-							(<div>{formik.errors.email}</div>) : null }
+							(<div className="error">{formik.errors.email}</div>) : null }
 
 						<input className="pw-input" id="password" type="password"
 						placeholder="  비밀번호를 입력하세요."  
 						{...formik.getFieldProps('password')}/>
 						{ formik.touched.password && formik.errors.password ? 
-							(<div>{formik.errors.password}</div>) : null }
+							(<div className="error">{formik.errors.password}</div>) : null }
 
 						<button className="loginbutton" type="submit">Login</button>
 					</form>
 				)}
 			</Formik>
+			<a className="registerService">
+				저희 서비스가 처음이시라면? 
+				<NavLink to="/register"> 회원가입</NavLink>
+			</a>
 		</div>
 		</>
 	)
