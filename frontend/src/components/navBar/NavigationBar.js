5a6bad68d8a799bb41135bcd9c2bf558e66bef8a
moonjiyoon <ske0830@gmail.com>
2021-12-09 22:55:59 +0900

    feat: 로그인여부에 따라 링크이동 여부 체크
    Former-commit-id: e1d9373dee747a65bda431f1fdff12510d323b90

--- frontend/src/components/navBar/NavigationBar.js
+++ frontend/src/components/navBar/NavigationBar.js
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import { useNavigate, NavLink } from 'react-router-dom';
 import "./NavigationBar.scss"
 import { logoutDB } from "../../login/LoginValidation";
+import { connect } from 'react-redux';
 
-const NavigationBar = () => {
+const NavigationBar = ({Login}) => {
   const navigate = useNavigate();
     const activeStyle = {
       width: "100px",
@@ -13,20 +14,29 @@ const NavigationBar = () => {
     return (
         <>
         <div className="navigation">
-            <NavLink className="menuBar" to="/calendar" 
-              style={({ isActive }) => isActive ? activeStyle : undefined}>
+            <NavLink className="menuBar" to={Login.login.is_login ? "/calendar" : "/login"}
+              style={({ isActive }) => 
+                isActive ? 
+                  Login.login.is_login ? activeStyle : undefined 
+                  : undefined}>
               <a className="navlink">달력</a>
               </NavLink>
             <div className="space"></div>
 
-            <NavLink className="menuBar" to="/diary-write" 
-              style={({ isActive }) => isActive ? activeStyle : undefined}>
+            <NavLink className="menuBar" to={Login.login.is_login ? "/diary-write" : "/login"}
+              style={({ isActive }) => 
+                isActive ? 
+                  Login.login.is_login ? activeStyle : undefined 
+                  : undefined}>
               <a className="navlink">일기</a>
               </NavLink>
             <div className="space"></div>
 
-            <NavLink className="menuBar" to="/flowerList" 
-              style={({ isActive }) => isActive ? activeStyle : undefined}>
+            <NavLink className="menuBar" to={Login.login.is_login ? "/flowerlist" : "/login"}
+              style={({ isActive }) => 
+                isActive ? 
+                  Login.login.is_login ? activeStyle : undefined 
+                  : undefined}>
               <a className="navlink">꽃무리</a>
               </NavLink>
             <div className="space"></div>
@@ -38,11 +48,18 @@ const NavigationBar = () => {
               </NavLink>
             <div className="space"></div>
 
-            <NavLink className="menuBar" to="/" 
-              style={({ isActive }) => isActive ? activeStyle : undefined} onClick={()=>logoutDB(navigate)}>
-              <a className="navlink">logout</a>
-              </NavLink>
-            <div className="space"></div>
+            {
+              Login.login.is_login ?  
+                <NavLink className="menuBar" to="/" 
+                  style={({ isActive }) => isActive ? activeStyle : undefined} onClick={()=>logoutDB(navigate)}>
+                  <a className="navlink">logout</a>
+                </NavLink>
+              : <NavLink className="menuBar" to="/login" 
+                  style={({ isActive }) => isActive ? activeStyle : undefined}>
+                  <a className="navlink">login</a>
+                </NavLink>
+            }
+
 
             {/* <NavLink className="menuBar" to="/team" 
               style={({ isActive }) => isActive ? activeStyle : undefined}>
@@ -54,4 +71,7 @@ const NavigationBar = () => {
     );
 }
 
-export default NavigationBar;
+function mapStateToProps(state) {
+  return {Login:state};
+}
+export default connect(mapStateToProps)(NavigationBar);
